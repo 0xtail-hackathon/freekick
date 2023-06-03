7561ff81d4f6ed309728cb5b5f94253e3f38493c
imysh578 <imysh578@gmail.com>
2023-06-04 00:01:10 +0900

    feat: signin button in test page

--- freekick-client/pages/test.tsx
+++ freekick-client/pages/test.tsx
@@ -1,12 +1,25 @@
+import { useWalletSelector } from "@/contexts/WalletSelectorContext";
 import styles from "./test.module.css";
 
 export default function Test() {
+    const { selector, modal, accounts, accountId } = useWalletSelector();
+
     async function handleOnClickTestButton() {
         console.log("Clicked");
+        console.log(accountId);
     }
+
+    const handleSignIn = () => {
+        modal.show();
+    };
 
     return (
         <div className={styles.buttons}>
+            {selector.isSignedIn() ? (
+                <button>{accountId}</button>
+            ) : (
+                <button onClick={handleSignIn}>Sign in</button>
+            )}
             <button onClick={handleOnClickTestButton}>Test</button>
         </div>
     );
